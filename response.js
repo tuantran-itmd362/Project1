1485f7be60dc45df9c9c00564844938882f9aaff
sparkkid1234 <tranquocanhtuan1997@gmail.com>
2019-10-01 18:41:32 -0500

    adding javascript file + pop up alert for successful sign up

---
+++ response.js
@@ -0,0 +1,4 @@
+function AlertScreen() {
+    alert("You've signed up for book club!! Ready to adventure into the world of knowledge? We'll email information on meetups shortly!");
+    return false;
+}
